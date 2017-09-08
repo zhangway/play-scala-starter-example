9dced411c39c0188cd087ad6a7d356bfcaf40358
Wei Zhang <djang.wei@gmail.com>
2017-09-08 21:58:26 +0200

    make bootstrap and jquery work

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,6 +1,8 @@
 package controllers
 
 import javax.inject._
+
+import play.api.i18n._
 import play.api.mvc._
 
 /**
@@ -8,7 +10,10 @@ import play.api.mvc._
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(
+  cc: ControllerComponents)
+  extends AbstractController(cc)
+  with I18nSupport{
 
   /**
    * Create an Action to render an HTML page with a welcome message.
@@ -16,9 +21,11 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index = Action {
+  def index = Action { implicit request =>
     //Ok(views.html.index("Your new application is ready."))
-    Ok("Hello Bergen")
+//    val messages: Messages = request.messages
+//    val message: String = messages("info.error")
+    Ok(views.html.hello("Bergen"))
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -11,3 +11,7 @@ scalaVersion := "2.12.2"
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.0.0" % Test
 libraryDependencies += "com.h2database" % "h2" % "1.4.194"
+
+libraryDependencies += "org.webjars" % "bootstrap" % "3.3.6"
+libraryDependencies += "org.webjars" % "jquery" % "2.2.4"
+
